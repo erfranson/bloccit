b9a9eb4a2fa2e723624ea2a172c78a7a651bc1d4
Ethan <ethanfranson2@gmail.com>
2016-03-08 02:08:55 -0700

    idempotence

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,10 @@ posts = Post.all
   )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by(title: "the man", body: "is buff")
+puts "#{Post.count}"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
