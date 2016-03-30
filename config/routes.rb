f29e2f6ab87e8c03a95336b117527debea65b64b
Ethan <ethanfranson2@gmail.com>
2016-03-30 13:05:02 -0600

    39complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
 resources :users, only: [:new, :create]
 resources :sessions, only: [:new, :create, :destroy]
-  get 'about' => 'welcome#about'
-
-  root 'welcome#index'
+get 'about' => 'welcome#about'
+get 'faq' => 'welcome#faq'
+root 'welcome#index'
 end
